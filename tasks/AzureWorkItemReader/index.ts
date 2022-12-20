e62b5cc61ce86f2d5d4f6e6e94af15e44dc071fe
Andy Edinborough <andy@edinborough.org>
2022-12-20 16:23:24 +0000

    remove casts; rely on implicit types

--- tasks/AzureWorkItemReader/index.ts
+++ tasks/AzureWorkItemReader/index.ts
@@ -1,42 +1,35 @@
-import tl = require('azure-pipelines-task-lib/task');
-import * as azdev from "azure-devops-node-api";
+import tl from 'azure-pipelines-task-lib/task';
+import azdev from "azure-devops-node-api";
 
 async function run() {
     try {
         const organization = tl.getInput('organization', true);
-        const pat = tl.getInput('pat', true)
-        const inputField: string | undefined = tl.getInput('field', true);
-        const pullRequestId: number | undefined = parseInt(<string>tl.getInput('pullRequestId', true));
+        const pat = tl.getInput('pat', true) ?? '';
+        const inputField = tl.getInput('field', true) ?? '';
+        const pullRequestId = parseInt(tl.getInput('pullRequestId', true) ?? '');
 
         const url = `https://dev.azure.com/${organization}`
-        const authHandler = azdev.getPersonalAccessTokenHandler(<string>pat);
+        const authHandler = azdev.getPersonalAccessTokenHandler(pat);
         const connection = new azdev.WebApi(url, authHandler);
         const pullRequestTrackApi = await connection.getGitApi();
         const pullRequestInfo = await pullRequestTrackApi.getPullRequestById(pullRequestId);
-        let workItemRefs = await pullRequestTrackApi.getPullRequestWorkItemRefs(<string>pullRequestInfo.repository?.id, pullRequestId);
-        let workItemId : number | undefined = undefined;
-        if (workItemRefs) {
-            var workItemIds = workItemRefs.map(wi => parseInt(<string>wi.id));
-            if (workItemIds.length == 0) {
-                tl.setResult(tl.TaskResult.Failed, "WorkItem not found for PullRequest");
-                return
-            } else{
-                workItemId = workItemIds[0]
-            }
+        const workItemRefs = await pullRequestTrackApi.getPullRequestWorkItemRefs(pullRequestInfo.repository?.id ?? '', pullRequestId);
+        const workItemId = parseInt(workItemRefs?.[0].id ?? '')
+ 
+        if (!workItemId) {
+            tl.setResult(tl.TaskResult.Failed, 'WorkItem not found for PullRequest');
+            return;
         }
+
         const workItemTrackingApi = await connection.getWorkItemTrackingApi();
-        const workItem = await workItemTrackingApi.getWorkItem(<number>workItemId);
-        let inputFieldValue : string | undefined = undefined;
-        if (workItem.fields) {
-            if (inputField!= undefined) {
-                inputFieldValue = workItem.fields[inputField]
-                console.log(inputFieldValue,inputField)
-            }
+        const workItem = await workItemTrackingApi.getWorkItem(workItemId);
+        const inputFieldValue =  workItem.fields?.[inputField]
+        if (inputField && inputFieldValue) {
+            console.log(inputField, inputFieldValue)
+            tl.setVariable(inputField, inputFieldValue)
         }
-        if(inputField != null){
-            tl.setVariable(inputField,<string>inputFieldValue)
-        }
-        tl.setResult(tl.TaskResult.Succeeded, "Task Successful");
+
+        tl.setResult(tl.TaskResult.Succeeded, 'Task Successful');
     }
     catch (err) {
         if (typeof err === 'object' && err !== null) {
